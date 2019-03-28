a7565d532a492631dd89d9ac95a2d12c18a3912b
Niko Häppölä <niko.happola@gmail.com>
2019-03-28 22:21:54 +0200

    Tietokanta Herokuun postgresql

--- application/__init__.py
+++ application/__init__.py
@@ -4,6 +4,12 @@ app = Flask(__name__)
 
 # tietokanta
 from flask_sqlalchemy import SQLAlchemy
+
+import os
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///players.db"
 app.config["SQLALCHEMY_ECHO"] = True
 
@@ -37,4 +43,7 @@ def load_user(user_id):
     return User.query.get(user_id)
 
 # luodaan tietokantataulut tarvittaessa
-db.create_all()
+try:   
+    db.create_all()
+except:
+    pass
