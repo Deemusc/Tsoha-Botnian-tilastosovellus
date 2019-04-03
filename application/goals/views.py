a925377d6c36570f8625943cb93cb4b8e2422ace
Niko Häppölä <niko.happola@gmail.com>
2019-04-03 20:07:55 +0300

    useampien maalien tietojen syöttäminen kehitteillä

--- application/goals/views.py
+++ application/goals/views.py
@@ -14,38 +14,41 @@ from application.players.models import Player
 def goals_add(game_id):
     error = None
     form = GoalForm(game_id = game_id)
-    print('form:', form)
+    game = Game.query.filter_by(id=game_id).first_or_404()
+    goalsToAdd = game.botnia_goals
+    print(goalsToAdd)
+    #print('form:', form)
     if form.validate_on_submit():
         try:
             s = Player.query.filter_by(number=form.scorer_number.data).first_or_404()
             a = Player.query.filter_by(number=form.assist_number.data).first_or_404()
             g = Goal(game_id=game_id, scorer_id=s.id, assist_id=a.id)
-            print('tietoja:', g)
+            #print('tietoja:', g)
             db.session.add(g)
             db.session.commit()
             flash("goal added")
         except Exception as e:
             error = e
-    return render_template("/goals/add.html", form = form, error = error)
-    #return redirect(url_for("goals_create", game_id = game_id)) 
+    return render_template("/goals/add.html", form = form, error = error, goalsToAdd=goalsToAdd)
+
 
 
 # ---------- tätä ei juuri nyt tarvita --------
-@app.route("/goals/<game_id>", methods=["GET", "POST"])
+#@app.route("/goals/<game_id>", methods=["GET", "POST"])
 #@login_required
-def goals_create(game_id):
-    error = None
-    form = GoalForm()
-    print('form:', form)
-    if form.validate_on_submit():
-        try:
-            s = Player.query.filter_by(number=form.scorer_number.data).first_or_404()
-            a = Player.query.filter_by(number=form.assist_number.data).first_or_404()
-            g = Goal(game_id=game_id, scorer_id=s.id, assist_id=a.id)
-            print('tietoja:', g)
-            db.session.add(g)
-            db.session.commit()
-            flash("goal added")
-        except Exception as e:
-            error = e
-    return render_template("/goals/add.html", form = form, error = error)
+#def goals_create(game_id):
+#    error = None
+#    form = GoalForm()
+#    print('form:', form)
+#    if form.validate_on_submit():
+#        try:
+#            s = Player.query.filter_by(number=form.scorer_number.data).first_or_404()
+#            a = Player.query.filter_by(number=form.assist_number.data).first_or_404()
+#            g = Goal(game_id=game_id, scorer_id=s.id, assist_id=a.id)
+#            print('tietoja:', g)
+#            db.session.add(g)
+#            db.session.commit()
+#            flash("goal added")
+#        except Exception as e:
+#            error = e
+#    return render_template("/goals/add.html", form = form, error = error)
