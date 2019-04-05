9baec5412483f084ce7ae4f5822949662b9e46a0
Niko Häppölä <niko.happola@gmail.com>
2019-04-05 12:46:33 +0300

    Muokattu monimutkaisinta sql-kyselyä edelleen.

--- application/players/models.py
+++ application/players/models.py
@@ -14,11 +14,11 @@ class Player(db.Model):
 
     @staticmethod
     def list_goal_scorers():
-        stmt = text("SELECT number, name, COUNT(scorer_id)"
+        stmt = text("SELECT Player.number, Player.name, COUNT(Goal.scorer_id)"
                     " FROM Player"
-                    " INNER JOIN Goal ON scorer_id=player.id"
-                    " GROUP BY scorer_id"
-                    " ORDER BY COUNT(scorer_id) DESC;")
+                    " INNER JOIN Goal ON Goal.scorer_id=Player.id"
+                    " GROUP BY Goal.scorer_id"
+                    " ORDER BY COUNT(Goal.scorer_id) DESC;")
         res = db.engine.execute(stmt)
 
         response = []
